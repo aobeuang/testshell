9cdd60642520e25e01931bde461594a22200e036
aobeuang <aob.attaporn@gmail.com>
2019-05-30 17:11:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="ISO-8859-1">
+<title>Page Title</title>
+</head>
+<body>
+
 <?php 
 
-$ss = shell_exec('/app/deploy.sh');
+// if (count($argv) != 3) {
+//     echo "ERROR: invalid parameters." . PHP_EOL . "ex. php export.php cps/file_name.xlsx filname_save.csv" . PHP_EOL;
+//     die();
+// }
 
-echo $ss;
+// $file_path = $argv[1];
 
+// // echo $file_path. PHP_EOL;
+// // die();
+
+
+// if(!file_exists($file_path)) {
+//     echo 'ERROR: '.$file_path.' not found' . PHP_EOL;
+//     die();
+// }
+
+echo "
+ __  __              ____ _       _               
+|  \/  | __ _  __ _ / ___(_)_ __ | |__   ___ _ __ 
+| |\/| |/ _` |/ _` | |   | | '_ \| '_ \ / _ \ '__|
+| |  | | (_| | (_| | |___| | |_) | | | |  __/ |   
+|_|  |_|\__,_|\__, |\____|_| .__/|_| |_|\___|_|   
+              |___/        |_|                    
+Have fun. Don't forget to bookmark this website :)";
 ?>
+
+</body>
+</html>
