61c9ee9e4679c2b545fd22ca04fde06a5130bc8d
aobeuang <aob.attaporn@gmail.com>
2019-05-30 16:32:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php 
-$file = '/app/deploy.sh';
-$fp = fopen($file, 'w');
-    fwrite($fp, $content);
-    fclose($fp);
-    chmod($file, 0777);  //changed to add the zero
-echo "ssss";
+
 $ss = shell_exec('/app/deploy.sh');
 
 echo $ss;
